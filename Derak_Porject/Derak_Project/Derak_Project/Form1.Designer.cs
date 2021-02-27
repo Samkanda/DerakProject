e4eb6a4d26d3443ca44c128c854c17798bf79eda
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2021-02-27 15:52:44 -0500

    Updated Menu Screen and added play button

--- Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
+++ Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
@@ -30,7 +30,7 @@ namespace Derak_Project
         private void InitializeComponent()
         {
             this.backgroundPictureBox = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button_play = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,22 +43,22 @@ namespace Derak_Project
             this.backgroundPictureBox.TabIndex = 0;
             this.backgroundPictureBox.TabStop = false;
             // 
-            // button1
+            // button_play
             // 
-            this.button1.Location = new System.Drawing.Point(329, 265);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 49);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Play";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button_play.Location = new System.Drawing.Point(329, 265);
+            this.button_play.Name = "button_play";
+            this.button_play.Size = new System.Drawing.Size(123, 49);
+            this.button_play.TabIndex = 1;
+            this.button_play.Text = "Play";
+            this.button_play.UseVisualStyleBackColor = true;
+            this.button_play.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_play);
             this.Controls.Add(this.backgroundPictureBox);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -70,7 +70,7 @@ namespace Derak_Project
         #endregion
 
         private System.Windows.Forms.PictureBox backgroundPictureBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_play;
     }
 }
 
