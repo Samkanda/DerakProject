00b8c8bd515cf6dc9dac5c133bbfdbabdee1af6d
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2021-02-27 14:55:42 -0500

    Images and main menu screen added

--- Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
+++ Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
@@ -30,22 +30,35 @@ namespace Derak_Project
         private void InitializeComponent()
         {
             this.backgroundPictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // backgroundPictureBox
             // 
+            this.backgroundPictureBox.Image = global::Derak_Project.Properties.Resources.Durak_Background;
             this.backgroundPictureBox.Location = new System.Drawing.Point(0, 1);
             this.backgroundPictureBox.Name = "backgroundPictureBox";
             this.backgroundPictureBox.Size = new System.Drawing.Size(801, 444);
             this.backgroundPictureBox.TabIndex = 0;
             this.backgroundPictureBox.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 265);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.backgroundPictureBox);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +70,7 @@ namespace Derak_Project
         #endregion
 
         private System.Windows.Forms.PictureBox backgroundPictureBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Derak_Porject/Derak_Project/Derak_Project/Form1.cs
+++ Derak_Porject/Derak_Project/Derak_Project/Form1.cs
@@ -16,5 +16,10 @@ namespace Derak_Project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
