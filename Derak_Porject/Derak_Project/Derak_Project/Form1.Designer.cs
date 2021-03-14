cee35712a127e3ad62ba1ec6a878c5a8da90238c
Navpreet Kanda <52259259+Samkanda@users.noreply.github.com>
2021-03-14 17:57:39 -0400

    Made the first steps for the card box

--- Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
+++ Derak_Porject/Derak_Project/Derak_Project/Form1.Designer.cs
@@ -29,9 +29,13 @@ namespace Derak_Project
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.backgroundPictureBox = new System.Windows.Forms.PictureBox();
             this.button_play = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDealCards = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // backgroundPictureBox
@@ -42,6 +46,8 @@ namespace Derak_Project
             this.backgroundPictureBox.Size = new System.Drawing.Size(801, 444);
             this.backgroundPictureBox.TabIndex = 0;
             this.backgroundPictureBox.TabStop = false;
+            this.backgroundPictureBox.Visible = false;
+            this.backgroundPictureBox.Click += new System.EventHandler(this.backgroundPictureBox_Click);
             // 
             // button_play
             // 
@@ -53,16 +59,40 @@ namespace Derak_Project
             this.button_play.UseVisualStyleBackColor = true;
             this.button_play.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 97);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(84, 114);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnDealCards
+            // 
+            this.btnDealCards.Location = new System.Drawing.Point(12, 249);
+            this.btnDealCards.Name = "btnDealCards";
+            this.btnDealCards.Size = new System.Drawing.Size(123, 49);
+            this.btnDealCards.TabIndex = 3;
+            this.btnDealCards.Text = "Deal Cards";
+            this.btnDealCards.UseVisualStyleBackColor = true;
+            this.btnDealCards.Click += new System.EventHandler(this.btnDealCards_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDealCards);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_play);
             this.Controls.Add(this.backgroundPictureBox);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.backgroundPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +101,8 @@ namespace Derak_Project
 
         private System.Windows.Forms.PictureBox backgroundPictureBox;
         private System.Windows.Forms.Button button_play;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnDealCards;
     }
 }
 
--- Derak_Porject/Derak_Project/Derak_Project/Form1.cs
+++ Derak_Porject/Derak_Project/Derak_Project/Form1.cs
@@ -15,6 +15,7 @@ namespace Derak_Project
         public Form1()
         {
             InitializeComponent();
+            pictures = new PictureBox[32];
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -23,6 +24,57 @@ namespace Derak_Project
             backgroundPictureBox.Refresh();
             backgroundPictureBox.Visible = true;
             button_play.Visible = false;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+           
+           
+        }
+
+        private void backgroundPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private PictureBox[] pictures;
+        public const string imagePath = @"../../images/";
+        private void btnDealCards_Click(object sender, EventArgs e)
+        {
+            CreateControls();
+            DisplayControls();
+        }
+
+        private void CreateControls()
+        {
+            for (var counter = 0; counter < 6; counter++)
+            {
+                var newPictureBox = new PictureBox();
+                newPictureBox.Width = 75;
+                newPictureBox.Height = 100;
+                pictures[counter] = SizeImage(newPictureBox, counter + 2);
+            }
+        }
+
+        private PictureBox SizeImage(PictureBox pb, int i)
+        {
+            Image img = Image.FromFile(imagePath + i.ToString() + "C.jpg");
+            pb.Image = img;
+            pb.SizeMode = PictureBoxSizeMode.CenterImage;
+            pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            //pb.MouseClick = //function 
+            return pb;
+        }
+
+        private void DisplayControls()
+        {
+            for (var counter = 0; counter < 5; counter ++)
+            {
+                pictures[counter].Left = ( counter * 10) + 100;
+                this.Controls.Add(pictures[counter]);
+            }
         }
     }
 }
