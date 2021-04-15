25600ae6eee3706f3643ec6e0cb93bc52351a93d
Shoaib <coolbrampton@gmail.com>
2021-04-15 11:01:31 -0400

    Auto stash before merge of "main" and "origin/main"

--- Derak_Porject/Derak_Project/DurakClient/Launcher.Designer.cs
+++ Derak_Porject/Derak_Project/DurakClient/Launcher.Designer.cs
@@ -32,15 +32,19 @@ namespace DurakClient
             this.btnStart = new System.Windows.Forms.Button();
             this.numComputers = new System.Windows.Forms.NumericUpDown();
             this.numHumans = new System.Windows.Forms.NumericUpDown();
+            this.lblGameTitle = new System.Windows.Forms.Label();
+            this.lblHumanPlayers = new System.Windows.Forms.Label();
+            this.lblComputerPlayers = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numComputers)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numHumans)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(440, 233);
+            this.btnStart.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(33, 202);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(64, 23);
+            this.btnStart.Size = new System.Drawing.Size(457, 43);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start Game";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -48,14 +52,15 @@ namespace DurakClient
             // 
             // numComputers
             // 
-            this.numComputers.Location = new System.Drawing.Point(440, 193);
+            this.numComputers.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numComputers.Location = new System.Drawing.Point(344, 155);
             this.numComputers.Maximum = new decimal(new int[] {
             5,
             0,
             0,
             0});
             this.numComputers.Name = "numComputers";
-            this.numComputers.Size = new System.Drawing.Size(64, 20);
+            this.numComputers.Size = new System.Drawing.Size(146, 27);
             this.numComputers.TabIndex = 1;
             this.numComputers.Value = new decimal(new int[] {
             1,
@@ -65,7 +70,8 @@ namespace DurakClient
             // 
             // numHumans
             // 
-            this.numHumans.Location = new System.Drawing.Point(440, 152);
+            this.numHumans.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numHumans.Location = new System.Drawing.Point(344, 116);
             this.numHumans.Maximum = new decimal(new int[] {
             6,
             0,
@@ -77,7 +83,7 @@ namespace DurakClient
             0,
             0});
             this.numHumans.Name = "numHumans";
-            this.numHumans.Size = new System.Drawing.Size(64, 20);
+            this.numHumans.Size = new System.Drawing.Size(146, 27);
             this.numHumans.TabIndex = 2;
             this.numHumans.Value = new decimal(new int[] {
             1,
@@ -85,11 +91,45 @@ namespace DurakClient
             0,
             0});
             // 
+            // lblGameTitle
+            // 
+            this.lblGameTitle.AutoSize = true;
+            this.lblGameTitle.Font = new System.Drawing.Font("Neo Sans", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameTitle.Location = new System.Drawing.Point(23, 23);
+            this.lblGameTitle.Name = "lblGameTitle";
+            this.lblGameTitle.Size = new System.Drawing.Size(485, 59);
+            this.lblGameTitle.TabIndex = 3;
+            this.lblGameTitle.Text = "Durak Game Launcher";
+            this.lblGameTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblHumanPlayers
+            // 
+            this.lblHumanPlayers.AutoSize = true;
+            this.lblHumanPlayers.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHumanPlayers.Location = new System.Drawing.Point(29, 118);
+            this.lblHumanPlayers.Name = "lblHumanPlayers";
+            this.lblHumanPlayers.Size = new System.Drawing.Size(255, 20);
+            this.lblHumanPlayers.TabIndex = 4;
+            this.lblHumanPlayers.Text = "Number of human players desired: ";
+            // 
+            // lblComputerPlayers
+            // 
+            this.lblComputerPlayers.AutoSize = true;
+            this.lblComputerPlayers.Font = new System.Drawing.Font("Neo Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComputerPlayers.Location = new System.Drawing.Point(29, 157);
+            this.lblComputerPlayers.Name = "lblComputerPlayers";
+            this.lblComputerPlayers.Size = new System.Drawing.Size(274, 20);
+            this.lblComputerPlayers.TabIndex = 5;
+            this.lblComputerPlayers.Text = "Number of computer players desired: ";
+            // 
             // Launcher
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(528, 273);
+            this.Controls.Add(this.lblComputerPlayers);
+            this.Controls.Add(this.lblHumanPlayers);
+            this.Controls.Add(this.lblGameTitle);
             this.Controls.Add(this.numHumans);
             this.Controls.Add(this.numComputers);
             this.Controls.Add(this.btnStart);
@@ -103,6 +143,7 @@ namespace DurakClient
             ((System.ComponentModel.ISupportInitialize)(this.numComputers)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numHumans)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +152,8 @@ namespace DurakClient
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.NumericUpDown numComputers;
         private System.Windows.Forms.NumericUpDown numHumans;
+        private System.Windows.Forms.Label lblGameTitle;
+        private System.Windows.Forms.Label lblHumanPlayers;
+        private System.Windows.Forms.Label lblComputerPlayers;
     }
 }
