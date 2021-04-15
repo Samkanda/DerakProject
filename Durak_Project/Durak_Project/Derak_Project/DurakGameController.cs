3f0d23d91e88761b1077d84ce1b3ee733e4ccbd8
silkyrie <32624451+silkyrie@users.noreply.github.com>
2021-04-15 15:47:53 -0400

    gameController Commented

--- Durak_Project/Durak_Project/Derak_Project/DurakGameController.cs
+++ Durak_Project/Durak_Project/Derak_Project/DurakGameController.cs
@@ -179,7 +179,7 @@ namespace Derak_Project
                 throw new InvalidPlayException("You cannot play anymore cards this turn");
             }
             //if the player is attacker
-            if(activePlayer.Role == DurakRole.Attacker)
+            if(activePlayer.Role == DurakRole.Attacker || activePlayer.Role == DurakRole.Extra)
             {
                 // used is only there to remember whether the card did pass a check
                 bool used = false;
@@ -307,47 +307,6 @@ namespace Derak_Project
                         throw new InvalidPlayException("You cannot do that");
                     }
                 }
-            }// if the players role is that of a supporting attacker
-            else if (activePlayer.Role == DurakRole.Extra)
-            {
-                // nearly identical to attack function
-
-                //remembers whether a card passed a check
-                bool used = false;
-                //if its possible to at all play an attack
-                if (playingField.Count < 6 && defender.Count > 0)
-                {
-                    if (playingField.Count == 0)
-                    {
-                        playingField.Add(new DurakBattle(cardPlayed));
-                        log += Environment.NewLine + " attacked with " + cardPlayed.ToString();
-                        used = true;
-                    } else
-                    {
-                        bool rankAvailable = false;
-                        foreach (DurakBattle front in playingField)
-                        {
-                            if (front.Attack.rank == cardPlayed.rank)
-                            {
-                                rankAvailable = true;
-                            }
-                            if (front.Defense != null && front.Defense.rank == cardPlayed.rank)
-                            {
-                                rankAvailable = true;
-                            }
-                        }
-                        if (rankAvailable)
-                        {
-                            playingField.Add(new DurakBattle(cardPlayed));
-                            log += Environment.NewLine + " attacked with " + cardPlayed.ToString();
-                            used = true;
-                        }
-                    }
-                }
-                if (!used)
-                {
-                    throw new InvalidPlayException("You cannot do that");
-                }
             }
         }
 
@@ -356,11 +315,16 @@ namespace Derak_Project
         /// </summary>
         private void EndOfTurn()
         {
+            //if there was a card freeze turn it off
             cardFreeze = false;
 
+            //keeps track of whether the active player lost a round
             bool loser = false;
+
+            //if they are defender
             if(activePlayer == defender)
             {
+                //if theres any unanswered attacks
                 foreach(DurakBattle bout in playingField)
                 {
                     if(bout.Defense == null)
@@ -370,10 +334,12 @@ namespace Derak_Project
                 }
                 if (loser)
                 {
+                    //pick up all cards in field
                     foreach (DurakBattle bout in playingField)
                     {
                         activePlayer.AddRange(bout.Retrieve());
                     }
+                    //log then start new round
                     playingField.Clear();
                     log += Environment.NewLine + Environment.NewLine + activePlayer.Name + " has ended their turn and has lost the round";
                     Deal(); 
@@ -382,7 +348,9 @@ namespace Derak_Project
             }
             else if (activePlayer == attacker)
             {
+                //default to true
                 loser = true;
+                //set to false if any of the attacks are unanswered
                 foreach (DurakBattle bout in playingField)
                 {
                     if (bout.Defense == null)
@@ -392,23 +360,27 @@ namespace Derak_Project
                 }
                 if (loser)
                 {
+                    //if they are the loser move all to discard
                     foreach (DurakBattle bout in playingField)
                     {
                         DiscardPile.AddRange(bout.Retrieve());
                     }
+
+                    //log then start new round
                     playingField.Clear();
                     log += Environment.NewLine + Environment.NewLine + activePlayer.Name + " has ended their turn and has lost the round";
                     Deal();
                     CalculateRoles();
                 }
             }
-
+            //if the game is continueing
             if(players.Count > 1)
             {
                 NewTurn();
             } 
             else
             {
+                //otherwise end the game
                 GameEndEvent(this, new EventArgs());
             }
         }
@@ -420,14 +392,17 @@ namespace Derak_Project
         /// <returns>The player based on location in array</returns>
         public DurakHand Next(DurakHand startPlayer)
         {
+            //find player sent
             if(players.Count > 0)
             {
                 for (int i = 0; i < players.Count; i++)
                 {
                     if (startPlayer == players[i])
                     {
+                        //grab player after in the list
                         if (i + 1 == players.Count)
                         {
+                            //if at end of list grab start of list
                             return players[0];
                         } else
                         {
@@ -435,6 +410,7 @@ namespace Derak_Project
                         }
                     }
                 }
+                //throw an error if its not found
                 throw new ArgumentOutOfRangeException("", "hand is not in game");
             }
             return null;
@@ -447,14 +423,17 @@ namespace Derak_Project
         /// <returns>The player based on index in array</returns>
         public DurakHand Previous(DurakHand startPlayer)
         {
+            //find player sent
             if (players.Count > 0)
             {
                 for (int i = 0; i < players.Count; i++)
                 {
                     if (startPlayer == players[i])
                     {
+                        //grab player after in the list
                         if (i - 1 < 0)
                         {
+                            // if at beginning grab the end
                             return players[players.Count - 1];
                         } else
                         {
@@ -462,6 +441,7 @@ namespace Derak_Project
                         }
                     }
                 }
+                //cant find target in the list
                 throw new ArgumentOutOfRangeException("", "hand is not in game");
             }
             return null;
@@ -472,8 +452,12 @@ namespace Derak_Project
         /// </summary>
         private void CalculateRoles()
         {
+            //double check that all players have drawn if they can
             Deal();
+            //keep track of who went out
             lastOut = new List<DurakHand>();
+
+            //remove all extras who went out this turn
             for (int i = 0; i < players.Count; i++)
             {
                 if (players[i].Count == 0 && players[i].Role == DurakRole.Extra)
@@ -482,19 +466,28 @@ namespace Derak_Project
                     players.RemoveAt(i);
                 }
             }
+            //move the active player if attacker/defender are going out
+            //if the active player leaves the game next/previous will throw unhandled exceptions
+            //this block stops that
             if (attacker != null && defender != null)
             {
+               
                 if(activePlayer == attacker)
                 {
+                    // if attacker then move it back so theres no issue
+                    //also if the defender goes out the attacker has to attack again on the next person
                     if (attacker.Count < 1 || defender.Count < 1)
                     {
                         activePlayer = Previous(activePlayer);
                     }
                 } else
                 {
+                    //if the defender goes out
                     if ( defender.Count < 1)
                     {
+                        //set activeplayer back 1
                         activePlayer = Previous(activePlayer);
+                        //if the attacker is also going out set it back one more
                         if (attacker.Count < 1)
                         {
                             activePlayer = Previous(activePlayer);
@@ -503,7 +496,7 @@ namespace Derak_Project
                 }
             }
             
-
+            //remove attacker/defender if neccesary
             for (int i = 0; i < players.Count; i++)
             {
                 if (players[i].Count == 0 && players[i].Role != DurakRole.Extra)
@@ -512,17 +505,18 @@ namespace Derak_Project
                     players.RemoveAt(i);
                 }
             }
-
-            log += Environment.NewLine + Environment.NewLine;
-            foreach (DurakHand nonFool in lastOut)
+            //log anyone leaving the game
+            if(lastOut.Count > 0)
             {
-                log += Environment.NewLine + nonFool.Name + " has left the game due to not having any cards";
+                log += Environment.NewLine + Environment.NewLine;
+                foreach (DurakHand nonFool in lastOut)
+                {
+                    log += Environment.NewLine + nonFool.Name + " has left the game due to not having any cards";
+                }
             }
-
-            foreach (DurakHand player in players)
-            {
-                player.Role = DurakRole.Extra;
-            }
+            
+            
+            //calc new attacker/defender
             SwitchRoles();
         }
 
@@ -531,10 +525,18 @@ namespace Derak_Project
         /// </summary>
         private void SwitchRoles()
         {
+            //set everyones roles to extra
+            foreach (DurakHand player in players)
+            {
+                player.Role = DurakRole.Extra;
+            }
+            //attacker is next player
             attacker = Next(activePlayer);
             attacker.Role = DurakRole.Attacker;
+            //defender is next next player
             defender = Next(Next(activePlayer));
             defender.Role = DurakRole.Defender;
+            //log it to sidebar
             log += Environment.NewLine + Environment.NewLine + Attacker.Name + " is the attacker and " + defender.Name + " is the defender";
         }
 
@@ -543,12 +545,16 @@ namespace Derak_Project
         /// </summary>
         private void NewTurn()
         {
+            //activeplayer is now next player
             activePlayer = Next(activePlayer);
+            //display new player
             log += Environment.NewLine + Environment.NewLine + activePlayer.Role + " | " + activePlayer.Name + Environment.NewLine +
                 "=============================";
-
+            //sort their hand
             activePlayer.Sort();
+            //update their info
             activePlayer.UpdateInfo(playingField, talon.suit, deck.Count);
+            //call their turn function
             activePlayer.TakeTurn();
         }
 
@@ -557,6 +563,7 @@ namespace Derak_Project
         /// </summary>
         public void Deal()
         {
+            //force all players to draw to minimum starting with active player
             DurakHand target = activePlayer;
             for (int i = 0; i < players.Count; i++)
             {
@@ -570,23 +577,29 @@ namespace Derak_Project
         /// </summary>
         public void StartGame()
         {
+            //display players
             foreach (DurakHand player in players)
             {
                 Console.WriteLine(player.ToString());
             }
+            //last check for players
             if(players.Count < 2)
             {
                 throw new Exception("Not Enough Players");
             }
+
+            //initial active player set
             activePlayer = players[0];
+
+            //randomize who goes first
             Random rnd = new Random();
-            
             for(int i = rnd.Next(players.Count);i > 0; i--)
             {
                 activePlayer = Next(activePlayer);
             }
-
+            //calculate startng roles
             CalculateRoles();
+            //start first turn
             NewTurn();
         }
         #endregion
